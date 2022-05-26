0b51770c9a409e543fa6977f5a3e60e07aa4aba1
unknown <melhungn@gmail.com>
2022-05-26 10:17:47 -0400

    intento de filtro

--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
-import { moviesFilter } from './data.js';
+import { genderFilter, moviesFilter } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 //Accediendo a la Data
 const ghibli = data.films;
-const prueba = ghibli[2].description;
+//const prueba = ghibli[2].description;
 let movieName = moviesFilter(ghibli);
  
+function tarjetaPeliculas (titulo){
+    return 
+    `<div> 
+        <h5>${titulo.title}</h5>
+    </div>`
+}
+
+
+
+function verPeliculas (peli){
+ let  todasLasPelis = "";
+ peli.forEach((titulo) => todasLasPelis += tarjetaPeliculas(titulo));
+return todasLasPelis;
+}
+
+console.log(verPeliculas(ghibli))
+
+console.log(movieName);
+
 console.log(ghibli);
 
-console.log(prueba);
-/*
+/*console.log(ghibli);
+
+
+
+
+
+
 const moviesGhibli = moviesFilter();
 
 const prueba = document.createElement("section");
