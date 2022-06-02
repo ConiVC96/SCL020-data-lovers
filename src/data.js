435f03fed7634b8df7b4633f571d74372e084fcd
unknown <melhungn@gmail.com>
2022-06-02 01:44:26 -0400

    filtro de az y za, html en js, casi filtro top5

--- src/data.js
+++ src/data.js
@@ -1,29 +1,20 @@
-//Filtrar peliculas por titulo
-export const moviesZAfilter = (movie, string) => {
-  let ordenZA = movie.sort((a, b) => ((a.title < b.title) ? 1 :-1));
-  if (string === 'A-Z') {
-    console-log(ordenZA)
-    //return ordenZA;
-  }
-  if (string === 'Z-A') {
-    return ordenZA.reverse();
-  }
-  console.log(ordenZA);
-  //return  ordenZA;
+ //Funciones para sortear el orden
+export const listAZ = (movie) => { //ordena de la AZ
+  let orderAZ = movie.sort((a, b) => (a.title > b.title) ? 1 :-1);
+    return orderAZ;
+}
+
+export const listZA = (movie) => { //ordena de la ZA
+  let orderZA = movie.sort((a, b) => (a.title < b.title) ? 1 :-1);
+    return orderZA;
 }
   
   
-  // export const moviesAZfilter = (movie) => {
-  //   let ordenAZ = movie.sort((a, b) => (a.title > b.title) ? 1 :-1);
 
-  //   return ordenAZ;
-  //   }
-
-
-//Filtrar por genero
-export const genderFilter = (character) =>{
-  const gender = character.filter((obj) => {
-    obj.gender === string;
-  });
-return gender;
-};
+// //Filtrar por genero
+// export const genderFilter = (character) =>{
+//   const gender = character.filter((obj) => {
+//     obj.gender === string;
+//   });
+// return gender;
+// };
--- src/main.js
+++ src/main.js
@@ -1,31 +1,100 @@
-import { genderFilter, moviesZAfilter } from './data.js';
+import { listZA,listAZ } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 //Accediendo a la Data
-const ghibli = data.films;
-//const prueba = ghibli[2].description;
-//let movieName = moviesFilter(ghibli);
- 
-function tarjetaPeliculas (titulo){
-    console.log(titulo);
-    return `<div> 
-        <h5>${titulo.title}</h5>
-        <p>${titulo.description}</p>
-        <p>${titulo["people"]
-        .map((people) => {
-          return `<li>${people}</li>`;
-        })}</p>
+const ghibli = data.films; //data de ghibli
+const top1 = ghibli[2];
+const top2 = ghibli[3];
+const top3 = ghibli[4];
+const top4 = ghibli[5];
+const top5 = ghibli[11];
+
+let ghibliTop5 = [top1, top2, top3, top4, top5];
+console.log(ghibliTop5);
+
+//Funcion que cargue el Top5
+// const moviesTop5 = () => {
+//     window.addEventListener("load", () => { 
+//         posterCard(ghibliTop5);
+// } 
+
+// Función para limpiar, tarjeta vacia
+const emptyCard = () => { 
+    document.getElementById("Prueba").innerHTML = "";
+}
+
+let empty = document.getElementById("Prueba");
+
+//Crear Tarjetita
+const posterCard = (movie) => { //Funcion para mostrar tarjetitas
+    for (let i = 0; i < movie.length; ++i) {
+        let cardMovie = `<section class="containerCard">
+        <section class="moviesCard">
+        <h2> ${movie[i].title} </h2>
+        <!--<div><p>${movie[i].description}</p></div> -->
+        <img class="imgCard" src="${movie[i].poster}">
+        </section>
+        </section>`;
+        empty.innerHTML += cardMovie;
+        console.log(movie);
+    };
+}
+
+
+//Elegir de la A-Z y de la Z-A
+let alfabeticOrder = document.getElementById("orderAZ");
+alfabeticOrder.addEventListener('change',  (e) => {
+    let userSelect = e.target.value;
+    emptyCard();
+        if (userSelect == "A-Z") { // Ordena de A a Z
+            let orderA = listAZ(ghibli);
+            posterCard(orderA);
+            return
+        } else if (userSelect == "Z-A") { // Ordena de Z a A
+            let orderZ = listZA(ghibli);
+            posterCard(orderZ);
+            return
+        } else {
+            alert("No es una opción valida");
+        }
+});
+
+// cargar todos los poster al cargar la página
+window.addEventListener("load", () => { 
+    posterCard(ghibli);
+});
+
+
+console.log(listAZ(ghibli));
+console.log(ghibli);
+console.log(nameZA);
+console.log(prueba1);
+
+
+
+
+
+
+// function tarjetaPeliculas (titulo){
+//     console.log(titulo);
+//     return `<div> 
+//         <h5>${titulo.title}</h5>
+//         <p>${titulo.description}</p>
+//         <p>${titulo["people"]
+//         .map((people) => {
+//           return `<li>${people}</li>`;
+//         })}</p>
        
-    </div>`
-}
+//     </div>`
+// }
 
 
 
-function verPeliculas (peli){
- let  todasLasPelis = "";
- peli.forEach((titulo) => todasLasPelis += tarjetaPeliculas(titulo));
-return todasLasPelis;
-}
+// function verPeliculas (peli){
+//  let  todasLasPelis = "";
+//  peli.forEach((titulo) => todasLasPelis += tarjetaPeliculas(titulo));
+// return todasLasPelis;
+// }
 
 /*
 document.getElementById("orderAZ").addEventListener("change", (event)=>{
@@ -40,21 +109,17 @@ document.getElementById("orderAZ").addEventListener("change", (event)=>{
     }
 }) */
 
-let cardGhibli = document.getElementById("prueba");
-const alfabeticOrder = document.querySelector('#orderAZ');
-alfabeticOrder.addEventListener('change', (event) => {
-  cardGhibli.innerHTML = '';
-  verPeliculas(moviesZAfilter(ghibli, alfabeticOrder.target.value));
-});
+// let cardGhibli = document.getElementById("prueba");
+// const alfabeticOrder = document.querySelector('#orderAZ');
+// alfabeticOrder.addEventListener('change', (event) => {
+//   cardGhibli.innerHTML = '';
+//   verPeliculas(moviesZAfilter(ghibli, alfabeticOrder.target.value));
+// });
 
 
 //console.log(genderFilter(ghibli));
 
-console.log(verPeliculas(ghibli));
-
-console.log(movieName);
-
-console.log(ghibli);
+//console.log(verPeliculas(ghibli));
 
 /*console.log(ghibli);
 
